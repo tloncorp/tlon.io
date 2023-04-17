db0ff7b37657d96a6ae8123c0e3423421b8bd0d1
é. urcades <edouard@urcad.es>
2023-04-17 10:58:22 -0400

    update: add terms + privacy

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			listStyleType: {
+				roman: 'upper-roman',
+			},
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
