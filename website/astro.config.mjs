4fa532452bf75276a987794fe3f8296881de215f
Anthony Bruno <anthonybruno@gmail.com>
2024-03-02 11:59:09 -0700

    Global utils and Sitemap support

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+
+import globals from "./src/utils/globals";
 
 // https://astro.build/config
 export default defineConfig({
+  site: globals.siteURL,
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
 });
