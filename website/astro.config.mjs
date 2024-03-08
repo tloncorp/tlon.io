c2651d9cda2ee77100e7ca83e96ece14c510f5a7
Anthony Bruno <anthonybruno@gmail.com>
2024-03-08 16:15:13 -0700

    Astro config cleanup

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from "astro/config";
 import { sanityIntegration } from "@sanity/astro";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import globals from "./src/utils/globals";
-
 import partytown from "@astrojs/partytown";
+
+import globals from "./src/utils/globals";
 
 // https://astro.build/config
 export default defineConfig({
