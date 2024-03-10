3be080c7d2d9ec644de8b734801c51cd85fc50a6
Anthony Bruno <anthonybruno@gmail.com>
2024-03-10 11:36:15 -0600

    Support for sanity vercel plugin

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {codeInput} from '@sanity/code-input'
+import {vercelDeployTool} from 'sanity-plugin-vercel-deploy'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
@@ -11,7 +12,7 @@ export default defineConfig({
   projectId: '4vy6phvk',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool(), codeInput()],
+  plugins: [structureTool(), visionTool(), codeInput(), vercelDeployTool()],
 
   schema: {
     types: schemaTypes,
