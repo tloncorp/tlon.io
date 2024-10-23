ade6135236565c122927c08a4f369fc2aca818cc
James Acklin <james.acklin@gmail.com>
2024-10-23 14:02:22 -0400

    sanity: update config

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -5,41 +5,79 @@ import {codeInput} from '@sanity/code-input'
 import {schemaTypes} from './schemaTypes'
 import {DocumentIcon} from '@sanity/icons'
 
-export default defineConfig({
-  name: 'default',
-  title: 'Tlon.io',
-
-  projectId: '4vy6phvk',
-  dataset: 'staging',
-
-  plugins: [structureTool({
-    structure: (S) => {
-        return S.list()
-          .title("Content")
-          .items([
-            // Our singleton type has a list item with a custom child
-            S.listItem()
-              .title("Global content")
-              .id("globalContent")
-              .icon(DocumentIcon)
-              .child(
+export default defineConfig([
+  {
+    name: 'default',
+    title: 'Tlon.io Production',
+    projectId: '4vy6phvk',
+    dataset: 'production',
+    basePath: '/production',
+    plugins: [
+      structureTool({
+        structure: (S) => {
+          return S.list()
+            .title('Content')
+            .items([
+              // Our singleton type has a list item with a custom child
+              S.listItem().title('Global content').id('globalContent').icon(DocumentIcon).child(
                 // Instead of rendering a list of documents, we render a single
                 // document, specifying the `documentId` manually to ensure
                 // that we're editing the single instance of the document
                 S.document()
-                  .schemaType("globalContent")
-                  .documentId("53c46b24-cf16-438f-9f9c-12537707112a")
+                  .schemaType('globalContent')
+                  .documentId('53c46b24-cf16-438f-9f9c-12537707112a'),
               ),
               S.documentTypeListItem('post'),
               S.documentTypeListItem('author'),
               S.documentTypeListItem('page'),
               S.documentTypeListItem('menu'),
               S.documentTypeListItem('homeCard'),
-          ])
-      },
-  }), visionTool(), codeInput()],
+            ])
+        },
+      }),
+      visionTool(),
+      codeInput(),
+    ],
 
-  schema: {
-    types: schemaTypes,
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    name: 'staging',
+    title: 'Tlon.io Staging',
+    projectId: '4vy6phvk',
+    dataset: 'staging',
+    basePath: '/staging',
+    plugins: [
+      structureTool({
+        structure: (S) => {
+          return S.list()
+            .title('Content')
+            .items([
+              // Our singleton type has a list item with a custom child
+              S.listItem().title('Global content').id('globalContent').icon(DocumentIcon).child(
+                // Instead of rendering a list of documents, we render a single
+                // document, specifying the `documentId` manually to ensure
+                // that we're editing the single instance of the document
+                S.document()
+                  .schemaType('globalContent')
+                  .documentId('53c46b24-cf16-438f-9f9c-12537707112a'),
+              ),
+              S.documentTypeListItem('post'),
+              S.documentTypeListItem('author'),
+              S.documentTypeListItem('page'),
+              S.documentTypeListItem('menu'),
+              S.documentTypeListItem('homeCard'),
+            ])
+        },
+      }),
+      visionTool(),
+      codeInput(),
+    ],
+
+    schema: {
+      types: schemaTypes,
+    },
+  },
+])
