97787e430abae9997022760d9a77fe7a4beca7de
James Acklin <james.acklin@gmail.com>
2025-02-26 10:38:18 -0500

    site: switch to client-side waitlist signup form

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="@sanity/astro/module" />
+
+interface ImportMetaEnv {
+  readonly siteName: string;
+  readonly siteUrl: string;
+  readonly PUBLIC_MAILCHIMP_SIGNUP: string;
+  readonly postsPerPage: number;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
